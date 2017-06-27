e0ae2b1a0465bd7bffbd94058d0e0c74eaf0cec8
Will <sir.sleepy93@gmail.com>
2017-06-27 11:03:56 -0500

    Fixed/added comments

--- solution.py
+++ solution.py
@@ -2,15 +2,22 @@ assignments = []
 rows = 'ABCDEFGHI'
 cols = '123456789'
 
-def cross(A, B): # needed here
+
+def cross(A, B): # needed here for setting up unit and peer lists
 	"Cross product of elements in A and elements in B."
 	return [a+n for a in A for n in B]
 
+
 def generate_diagonals():
+	# Get the zip of rows with cols (which will be the left to right diagonal)
 	A = [r+c for (r,c) in zip(rows, cols)]
+	# Get the zip of rows with cols reversed (which will be the right to left diagonal)
 	B = [r+c for (r,c) in zip(rows, cols[::-1])]
+	# Return the lists
 	return [A, B]
 
+
+# Setup unit and peer lists here
 boxes = cross(rows, cols)
 
 row_units = [cross(r, cols) for r in rows]
@@ -22,7 +29,6 @@ units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
 
-# Function Declarations
 def assign_value(values, box, value):
 	"""
 	Please use this function to update your values dictionary!
@@ -38,6 +44,7 @@ def assign_value(values, box, value):
 		assignments.append(values.copy())
 	return values
 
+
 def naked_twins(values):
 	"""
 	Eliminate values using the naked twins strategy.
@@ -47,27 +54,34 @@ def naked_twins(values):
         the values dictionary with the naked twins eliminated from peers.
     """
 
+	# Generate a list with all boxes of length two (pairs)
 	pairlist = [box for box in values.keys() if len(values[box]) == 2]
 	twinlist = []
-	for box1 in pairlist:
-		for box2 in peers[box1]:
-			if set(values[box1]) == set(values[box2]) and (box2, box1) not in twinlist:
-				twinlist.append((box1, box2))
+	for pair in pairlist:
+		for peer in peers[pair]:
+			# For each peer of a pair in pairlist: Check if the peer has the same value as the pair
+			# and check that the reverse tuple is not already added before adding to twinlist
+			if set(values[pair]) == set(values[peer]) and (peer, pair) not in twinlist:
+				twinlist.append((pair, peer))
 
 	# Eliminate the naked twins as possibilities for their peers
 	for twins in twinlist:
-		# get only mutual peers
+		# Get only mutual peers
 		mutual = set(peers[twins[0]]) & set(peers[twins[1]])
 
-		# since we know they are equal
+		# Since we twins[0] and twins[1] are equal
 		twin = twins[0]
 
+		# For each mutual peer: if the peer box contians more than one value, and
+		# the peer box is not in our twinlist then remove the values
+		# of our naked twins from that peer
 		for peer in mutual:
 			if len(values[peer]) > 1 and peer not in twinlist:
 				for i in values[twin]:
 					assign_value(values, peer, values[peer].replace(i, ''))
-	#print(values)
+
 	return values
+
 
 def grid_values(grid):
 	"""
@@ -80,14 +94,20 @@ def grid_values(grid):
 			Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
 	"""
 	# Taken from Strategy 1: Elimination lesson
+
+	all_values = '123456789'
+
+	# For each character in the grid string: if the char is '.' fill in all values
+	# else just use the character
 	values = []
 	for box in grid:
 		if box == '.':
-			values.append(cols) # cols being 1 - 9
-		elif box in cols:
+			values.append(all_values)
+		elif box in all_values:
 			values.append(box)
 	assert len(values) == 81
 	return dict(zip(boxes, values))
+
 
 def display(values):
 	"""
@@ -96,6 +116,7 @@ def display(values):
 		values(dict): The sudoku in dictionary form
 	"""
 	# Taken from Strategy 1: Elimination lesson
+	# Print all boxes with lines in proper place.
 	boxes = cross(rows, cols)
 	width = 1 + max(len(values[box]) for box in boxes)
 	line = '+'.join(['-'*(width*3)]*3)
@@ -105,66 +126,88 @@ def display(values):
 		if row in 'CF': print(line)
 	return
 
+
 def eliminate(values):
-	solved = [key for key in values.keys() if len(values[key]) == 1]
+	# Get the number of cells of length 1 (get all solved cells)
+	solved = [box for box in values.keys() if len(values[box]) == 1]
 	for idx in solved:
 		for peer in peers[idx]:
-
+			# For each peer of a solved box remove the value of the solved box from the peer
 			assign_value(values, peer, values[peer].replace(values[idx], ''))
-			#values[peer] = values[peer].replace(values[idx], '')
+
 	return values
 
+
 def only_choice(values):
+	all_values = '123456789'
+
 	for unit in unitlist:
-		for i in cols:
+		for i in all_values:
+			# For each value in all_values: if the value only appears in one box in a unit,
+			# remove all other values from that box (i.e. assign that value)
 			place = [box for box in unit if i in values[box]]
 			if len(place) == 1:
 				assign_value(values, place[0], i)
-				#values[place[0]] = i
+
 	return values
 
 
-def solved(values, length = 1):
+def solved_to_length(values, length=1):
 	# Return number of boxes with number of possibilities == length
+	# Default this to 1 because we search for that length most often.
 	return len([box for box in values.keys() if len(values[box]) == length])
+
 
 def reduce_puzzle(values):
 	stalled = False
-	#print("REDUCING")
-	while not stalled:
-		solved_before = solved(values)
-		#print("Solved before: ", solved_before)
 
+	while not stalled:
+		# Get all values of length == 1 before reducing puzzle
+		solved_before = solved_to_length(values)
+
+		# Reduce puzzle by calling all of our reducing methods
 		values = eliminate(values)
 		values = only_choice(values)
 		values = naked_twins(values)
 
-		solved_after = solved(values)
-		#print("Solved after: ", solved_after)
+		# Get all values of length == 1 after reducing the puzzle
+		solved_after = solved_to_length(values)
 
+		# If the number solved has not changed, we have reduced the puzzle as much
+		# as possible this go round (i.e. will exit the loop after this iteration)
 		stalled = solved_before == solved_after
 
-		if solved(values, 0):
+		# If any of our boxes have no value something has gone horribly wrong
+		if solved_to_length(values, 0):
 			return False
+
 	return values
 
+
 def search(values):
+	# Reduce the puzzle as much as we can first.
 	values = reduce_puzzle(values)
 
+	# If values is Flase, then reduce_puzzle() broke
 	if values is False:
 		return False
+	# Else if all boxes have length of 1, then the puzzle is solved
 	elif all(len(values[box]) == 1 for box in boxes):
 		return values
 
+	# To search find the box with the lowest number of possibilities (hopefully close to two)
 	_, box = min((len(values[box]), box) for box in boxes if len(values[box]) > 1)
 
+	# For each value in that minimum possibilities box, make a temporary copy
+	# and assign that value to the box and search
+	# If the search returns False then then try one of the other possible values until a valid puzzle is returned.
 	for value in values[box]:
-		#print("SEARCHING")
 		puzzle = values.copy()
 		puzzle[box] = value
 		run = search(puzzle)
 		if run:
 			return run
+
 
 def solve(grid):
 	"""
@@ -175,16 +218,14 @@ def solve(grid):
 	Returns:
 		The dictionary representation of the final sudoku grid. False if no solution exists.
 	"""
+	# Get the grid setup and dive in to the search.
 	return search(grid_values(grid))
 
-def set_diagonal_mode(switch=True):
-	unitlist = row_units + column_units + square_units
 
-	unitlist = unitlist + diagonal_units if switch else unitlist
+if __name__ == '__main__':
+	grid3 = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
 
-	return
-
-def run_pygame():
+	display(solve(grid3))
 	try:
 		from visualize import visualize_assignments
 		visualize_assignments(assignments)
@@ -193,10 +234,3 @@ def run_pygame():
 		pass
 	except:
 		print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
-
-
-if __name__ == '__main__':
-	grid3 = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
-
-	display(solve(grid3))
-	run_pygame()
